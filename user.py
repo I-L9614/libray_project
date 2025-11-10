125631651e4fa037828c175f5dc18a0e0a31b1d7
Yechiel <springer058@gmail.com>
2025-11-10 12:15:37 +0200

    class user

---
+++ user.py
@@ -0,0 +1,19 @@
+from book import Book
+
+class User:
+    count = 1
+    def __init__(self,name):
+        self.name = name
+        self.id = User.count
+        self.borrowed_books = []
+        User.count += 1
+
+    def add_to_borrowed(self,isbn):
+        self.borrowed_books.append(isbn)
+
+    def remuv_from_borrowed(self,isbn):
+        self.borrowed_books.remove(isbn)
+
+
+
+        
