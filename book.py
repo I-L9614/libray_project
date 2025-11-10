1c7ceef7a860531d2dfbbb543515949a081f0b67
Yechiel <springer058@gmail.com>
2025-11-10 11:33:18 +0200

    class book

--- book.py
+++ book.py
@@ -1,2 +1,15 @@
 class Book:
-    
+    count = 10000
+    def __init__(self,title,author):
+        self.title = title
+        self.author = author
+        self.ISBN = Book.count
+        Book.count += 2
+        self.is_available = True
+
+    def apdet_availabileti(self):
+        self.is_available = not self.is_available   
+
+    def __str__(self):
+        print(f"title: {self.title}.author: {self.author}. ISBN: {self.ISBN}. is_available: {self.is_available}.")
+
