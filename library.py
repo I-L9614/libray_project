64664af80b63a2709b8405f19a2af7752027f207
I-L9614 <leviitamar034@gmail.com>
2025-11-11 10:17:40 +0200

    fixed few thing in Itamar - Mark

--- library.py
+++ library.py
@@ -1,5 +1,5 @@
-from book import *
-from user import *
+# from book import Book
+# from user import User
 
 class Librey:
     def __init__(self, list_of_books, list_of_users):
@@ -16,10 +16,11 @@ class Librey:
         self.users.append(user)
         print(f"New user registered. user info: {user}")
 
+    # TODO: use book instance and user instance
     def borow_book(self, user_id, book_id):
         print(f"book:{book_id},has ben borrowed to user: {user_id}")
         
-    
+    # TODO: use book instance and user instance
     def return_book(self, user_id, book_id):
         print(f"user: {user_id} has return book: {user_id}")
     
@@ -30,16 +31,20 @@ class Librey:
                 availabl_books.append(i)
         return availabl_books
     
+    # TODO: Improve
     def look_for_book():
         return input("enter book title")
     
+    # TODO: Improve
     def serch_book(self, titel):
         for i in self.books:
             if i.title == titel:
                 return i
             else:
+                print("book not found")
                 continue
     
+    # TODO: Improve
     def librery_menu():
         return input("anter choise:\n1.Borow book\n2.return book\n3.add book")
     
